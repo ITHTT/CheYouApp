bf0ff23fdfcdbcf93bcdbc05aefaffebf63f6ee8
ithtt <htt123456>
2018-01-23 21:36:18 +0800

    我的页面

--- app/src/main/java/com/hw/cy/app/view/fragment/MainMineFragment.java
+++ app/src/main/java/com/hw/cy/app/view/fragment/MainMineFragment.java
@@ -1,16 +1,24 @@
 package com.hw.cy.app.view.fragment;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.hw.cy.app.R;
 import com.hw.cy.app.base.BaseFragment;
+import com.hw.cy.app.util.StatusBarUtil;
+
+import butterknife.BindView;
 
 /**
  * Created by ithtt on 2018/1/22.
  */
 
 public class MainMineFragment extends BaseFragment{
+    @BindView(R.id.layout_user_profile)
+    FrameLayout layoutUserProfile;
+
     @Override
     public int getContentLayoutId() {
         return R.layout.fragment_main_mine;
@@ -18,6 +26,7 @@ public class MainMineFragment extends BaseFragment{
 
     @Override
     public void initViewData(Intent intent, Bundle saved) {
+        initView();
 
     }
 
@@ -29,5 +38,11 @@ public class MainMineFragment extends BaseFragment{
     @Override
     public Object newPersenter() {
         return null;
+    }
+
+    private void initView(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT) {
+            layoutUserProfile.setPadding(0,StatusBarUtil.getStatusBarHeight(getActivity()),0,0);
+        }
     }
 }
